ef6a14c14d724de9e1106b88a2f6fc390638e2b8
andreped <andrped94@gmail.com>
2022-03-31 22:18:44 +0200

    added working real-time image classification app example. Runs about 4.5 FPS on the emulator Android phone.

---
+++ classification-streamer/widgets/recognition.dart
@@ -0,0 +1,186 @@
+import 'dart:async';
+import '../services/tensorflow-service.dart';
+import 'package:flutter/material.dart';
+
+class Recognition extends StatefulWidget {
+  Recognition({Key? key, required this.ready}) : super(key: key);
+
+  // indicates if the animation is finished to start streaming (for better performance)
+  final bool ready;
+
+  @override
+  _RecognitionState createState() => _RecognitionState();
+}
+
+// to track the subscription state during the lifecicle of the component
+enum SubscriptionState { Active, Done }
+
+class _RecognitionState extends State<Recognition> {
+  // current list of recognition
+  List<dynamic> _currentRecognition = [];
+
+  // listens the changes in tensorflow recognitions
+  StreamSubscription? _streamSubscription;
+
+  // initialize FPS
+  String fpsValue = "0.0";
+  double count = 0;
+  double timer = 0;
+
+  // tensorflow service injection
+  TensorflowService _tensorflowService = TensorflowService();
+
+  // to track FPS
+  Stopwatch stopwatch = Stopwatch()..start();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // starts the streaming to tensorflow results
+    _startRecognitionStreaming();
+  }
+
+  _startRecognitionStreaming() {
+    _streamSubscription ??= _tensorflowService.recognitionStream.listen((recognition) {
+        if (recognition != null) {
+          // rebuilds the screen with the new recognitions
+          setState(() {
+            _currentRecognition = recognition;
+          });
+        } else {
+          _currentRecognition = [];
+        }
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFF120320),
+                ),
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                  children: widget.ready
+                      ? <Widget>[
+                          // shows recognition title
+                          _titleWidget(),
+
+                          // shows recognitions list
+                          _contentWidget(),
+                        ]
+                      : <Widget>[],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _titleWidget() {
+
+    if (_currentRecognition.isNotEmpty) {
+      // measure fps based on a running average
+      count++;
+      //timer = (timer * (count - 1) + 1.0 / (stopwatch.elapsedMilliseconds.toDouble() / 1000)) / count;
+
+      // measure fps
+      //fpsValue = timer.toStringAsFixed(1);
+
+      fpsValue = (1.0 / (stopwatch.elapsedMilliseconds.toDouble() / 1000)).toStringAsFixed(1);
+
+      // reset stopwatch
+      stopwatch.stop();
+      stopwatch.reset();
+      stopwatch.start();
+    }
+
+    return Container(
+      padding: EdgeInsets.only(top: 15, left: 20, right: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            "Recognitions, FPS: " + fpsValue,
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _contentWidget() {
+    var _width = MediaQuery.of(context).size.width;
+    var _padding = 20.0;
+    var _labelWitdth = 150.0;
+    var _labelConfidence = 30.0;
+    var _barWitdth = _width - _labelWitdth - _labelConfidence - _padding * 2.0;
+
+    if (_currentRecognition.isNotEmpty) {
+      return Container(
+        height: 150,
+        child: ListView.builder(
+          itemCount: _currentRecognition.length,
+          itemBuilder: (context, index) {
+            if (_currentRecognition.length > index) {
+              return Container(
+                height: 40,
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.only(left: _padding, right: _padding),
+                      width: _labelWitdth,
+                      child: Text(
+                        _currentRecognition[index]['label'],
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Container(
+                      width: _barWitdth,
+                      child: LinearProgressIndicator(
+                        backgroundColor: Colors.transparent,
+                        value: _currentRecognition[index]['confidence'],
+                      ),
+                    ),
+                    Container(
+                      width: _labelConfidence,
+                      child: Text(
+                        (_currentRecognition[index]['confidence'] * 100).toStringAsFixed(0) + '%',
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Container();
+            }
+          },
+        ),
+      );
+    } else {
+      return Text('');
+    }
+  }
+
+  @override
+  void dispose() {
+    _streamSubscription?.cancel();
+    super.dispose();
+  }
+}
